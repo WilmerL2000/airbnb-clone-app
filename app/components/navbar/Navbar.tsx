6d110b184519bccd0dd58d34f9699a37ef1bc7a6
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-06-01 13:52:16 -0600

    Initial structure

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">Navbar</div>
+    </div>
+  );
+}
